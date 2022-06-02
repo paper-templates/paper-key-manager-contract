9b75b069735884f34df8d6db98c8cd5953562fe0
ElasticBottle <winstonyeo99@yahoo.com>
2022-06-02 13:39:29 -0700

    Change Paper Verification to handle various data input

--- verification/PaperVerification.sol
+++ verification/PaperVerification.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: APACHE-2.0
 pragma solidity >=0.8.9 <0.9.0;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
-import "./structs/PaperMintData.sol";
 
 contract PaperVerification is EIP712("Paper", "1") {
     address private paperKey;
@@ -12,9 +12,13 @@ contract PaperVerification is EIP712("Paper", "1") {
         paperKey = _paperKey;
     }
 
-    modifier onlyPaper(PaperMintData.MintData calldata _data) {
-        _checkValidity(_data, paperKey);
-        minted[_data.nonce] = true;
+    modifier onlyPaper(
+        bytes memory _data,
+        bytes32 _nonce,
+        bytes calldata _signature
+    ) {
+        _checkValidity(_data, _nonce, _signature, paperKey);
+        minted[_nonce] = true;
         _;
     }
 
@@ -22,14 +26,16 @@ contract PaperVerification is EIP712("Paper", "1") {
     /// @dev Will revert if the signature is invalid i.e. not the paperKey passed in the constructor. Does not verify that the signer (paperKey) is authorized to mint NFTs.
     /// @param _data MintData describing the transaction details.
     function _checkValidity(
-        PaperMintData.MintData calldata _data,
+        bytes memory _data,
+        bytes32 _nonce,
+        bytes calldata _signature,
         address _paperKey
     ) internal view {
-        bytes32 digest = _hashTypedDataV4(PaperMintData.hashData(_data));
-        address signer = ECDSA.recover(digest, _data.signature);
+        bytes32 digest = _hashTypedDataV4(keccak256(_data));
+        address signer = ECDSA.recover(digest, _signature);
         require(signer == _paperKey, "Invalid signature");
         // make sure that the signature has not been used before
-        require(!isMinted(_data.nonce), "Mint request already processed");
+        require(!isMinted(_nonce), "Mint request already processed");
     }
 
     function isMinted(bytes32 nonce) private view returns (bool) {
